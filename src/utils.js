66384b70ee8881f3577fdf51531b9f355ba9c9c9
stephenwyang <stephen.cr.yang@gmail.com>
2024-12-02 10:39:56 -0800

    Code cleanup/Setup

---
+++ src/utils.js
@@ -0,0 +1,6 @@
+/* ` is going to be used instead of quotes to allow for string literals (inserting variables) */
+
+export const getImageUrl = (path) => {
+    return new URL(`/assets/${path}`, import.meta.url).href
+}
+
