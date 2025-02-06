6b2b3cafd34c64cf506c3924202a402c8adad97c
stephenwyang <stephen.cr.yang@gmail.com>
2025-02-06 00:44:06 -0800

    Navbar in progress

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,7 @@
 /* ` is going to be used instead of quotes to allow for string literals (inserting variables) */
 
 export const getImageUrl = (path) => {
-    return new URL(`/assets/${path}`, import.meta.url).href
-}
+    /* return new URL(`/assets/${path}`, import.meta.url).href; */
+    return `/assets/${path}`;
+};
 
