e091cc3466ef3c61b3dad41723e445fe73b86b02
stephenwyang <stephen.cr.yang@gmail.com>
2025-04-02 23:36:21 -0700

    Trying out things without referring to the tutorial

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,6 +3,10 @@ import styles from "./About.module.css"
 
 export const About = () => {
     return <section className={styles.container}>
-
+        <div className={styles.infobox}>
+            TODO: ADD BOX, UPDATE ABOUT WITH SOME GENERAL INFORMATION
+            {/* Section = large part, make div the black box?
+                We can add <p> and others as the internal text */}
+        </div>
     </section>;
 }
