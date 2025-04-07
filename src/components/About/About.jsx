3affaf9490c11ddec03aa5e7370c6fa7b4c4ec79
stephenwyang <stephen.cr.yang@gmail.com>
2025-04-06 23:25:08 -0700

    Followed general guidelines, finished About section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,11 +2,38 @@ import React from "react"
 import styles from "./About.module.css"
 
 export const About = () => {
-    return <section className={styles.container}>
-        <div className={styles.infobox}>
-            TODO: ADD BOX, UPDATE ABOUT WITH SOME GENERAL INFORMATION
-            {/* Section = large part, make div the black box?
-                We can add <p> and others as the internal text */}
+    return <section className={styles.container} id="about">
+        <h2 className={styles.title}>About Me</h2>
+        <div className={styles.content}>
+            <ul className={styles.aboutItems}>
+                <li className={styles.aboutItem}>
+                    <div className = {styles.aboutItemText}>
+                        <h3>Background</h3>
+                        <p>
+                            I am a software developer located in the SF Bay Area, 
+                            loooking for a full-time position while I expand my knowledge in the field.                        
+                        </p>
+                    </div>
+                </li>
+                <li className={styles.aboutItem}>
+                    <div className = {styles.aboutItemText}>
+                        <h3>Education</h3>
+                        <p>
+                            I recently graduated from the Georgia Institute of Technology 
+                            with a Master's Degree in Computer Science, focused on Machine Learning.                        
+                            I also do have Bachelor's Degree for Computer Science from San Jose State University.
+                        </p>
+                    </div>
+                </li>
+                <li className={styles.aboutItem}>
+                    <div className = {styles.aboutItemText}>
+                        <h3>Backend Developer</h3>
+                        <p>
+                            I have some experience developing optimized back-end systems with some of my projects.                        
+                        </p>
+                    </div>
+                </li>
+            </ul>
         </div>
     </section>;
 }
