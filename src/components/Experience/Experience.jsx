28bb7f8a5e9eb63ce3791ffcdc09cb1c71430b77
stephenwyang <stephen.cr.yang@gmail.com>
2025-04-18 00:15:41 -0700

    Filled out some json files

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,6 +1,16 @@
 import React from "react"
 import styles from "./Experience.module.css"
+import { getImageUrl } from "../../utils"
+
+import skills from "../../data/skills.json"
+import history from "../../data/history.json"
 
 export const Experience = () => {
-
+    return <section id="experience">
+        <h2>Experience</h2>
+        <div>
+            <div></div>
+            <ul></ul>
+        </div>
+    </section>
 };
