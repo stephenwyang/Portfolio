055f950fa302dc8666f341b86b31e9f8bd5bebc3
stephenwyang <stephen.cr.yang@gmail.com>
2025-05-02 02:29:11 -0700

    Finished with outline for Projects, will touch-up later

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -3,6 +3,24 @@ import React from "react";
 import styles from "./ProjectCard.module.css";
 import { getImageUrl } from "../../utils";
 
-export const ProjectCard = ({ project }) => {
-
+{/* From what I understood, we're basically passing in a project object
+    /extracting out the components from the json file */}
+export const ProjectCard = ({ project: {title, imageSrc, description, skills, source} }) => {
+    return (
+        <div className={styles.container}>
+            <img 
+                src={getImageUrl(imageSrc)} 
+                alt={`${title}'s Image`} />
+            <h3 className = {styles.title}>{title}</h3>
+            <p className = {styles.description}>{description}</p>
+            <ul className = {styles.skills}>
+                {skills.map((skill, id) => {
+                    return <li key={id} className = {styles.skill}>{skill}</li>;
+                })}
+            </ul>
+            <div className = {styles.links}>
+                <a href={source} className = {styles.link}>Source</a>
+            </div>
+        </div>
+    );
 };
