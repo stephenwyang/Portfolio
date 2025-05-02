f466186ba5b4b599c15bc4d0a53c291120c41966
stephenwyang <stephen.cr.yang@gmail.com>
2025-05-01 23:02:45 -0700

    Updated json and working on finishing Projects section

---
+++ src/components/Projects/ProjectCard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+import styles from "./ProjectCard.module.css";
+import { getImageUrl } from "../../utils";
+
+export const ProjectCard = ({ project }) => {
+
+};
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,7 +1,20 @@
 import React from "react"
 import styles from "./Projects.module.css"
+
+import projects from "../../data/projects.json"
+import { ProjectCard } from "./ProjectCard"
 import { getImageUrl } from "../../utils"
 
 export const Projects = () => {
-
-}
+    return (
+        <section className={styles.container} id="projects">
+            <h2 className={styles.title}>Projects</h2>
+            <div className = {styles.projects}>
+                {/* Code for ProjectCard is reusable so we're making it a component instead */}
+                {projects.map((project, id) => {
+                    return <ProjectCard key={id} project={project} />;
+                })}
+            </div>
+        </section>
+    );
+};
